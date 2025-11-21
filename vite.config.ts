762c1c04d5aea7965b73951c0f9dfc0b6815618d
SHOBU SHOGO <120190757+komakamo@users.noreply.github.com>
2025-11-21 16:16:32 +0900

    Configure GitHub Pages build output

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Pawapuro-2-Online/',
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
 });
